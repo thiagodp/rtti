6a5d29ee95f4080ef899f8d581a38a8b63fb62b2
thiagodp <thiago-dp@bol.com.br>
2015-10-22 19:33:46 -0200

    @new First commit;

---
+++ README.md
@@ -0,0 +1,36 @@
+# RTTI
+
+A Run-Time Type Information extractor, useful for getting/setting private attributes from/to objects.
+
+Current [version](http://semver.org/): `1.0` (stable, used in production code)
+
+Classes:
+
+* [phputil\RTTI](https://github.com/thiagodp/rtti/blob/master/lib/RTTI.php)
+
+### Installation
+
+```command
+composer require phputil/rtti
+```
+
+### Example
+
+Extracting all attributes from a class (even `private` or `protected`).
+
+```php
+<?php
+require_once 'vendor/autoload.php'; // or 'RTTI.php' when not using composer
+
+use phputil\RTTI;
+
+class User {
+	private $name;
+	function __construct( $n ) { $this->name = $n; }
+	function getName() { return $this->name; }
+}
+
+// array( 'user' => 'Bob' )
+var_dump( RTTI::getAttributes( new User( 'Bob' ), RTTI::allFlags() ) );
+?>
+```
---
+++ lib/RTTI.php
@@ -0,0 +1,214 @@
+<?php
+namespace phputil;
+
+/**
+ * Run time type information utilities.
+ *
+ * @author	Thiago Delgado Pinto
+ * @version	1.0
+ */
+class RTTI {
+	
+	const IS_PRIVATE	= \ReflectionProperty::IS_PRIVATE;
+	const IS_PROTECTED	= \ReflectionProperty::IS_PROTECTED;
+	const IS_PUBLIC		= \ReflectionProperty::IS_PUBLIC;
+	
+	
+	/**
+	 * Return all the visibility flags.
+	 * @return int
+	 */
+	static function allFlags() { 
+		return \ReflectionProperty::IS_PRIVATE
+			|  \ReflectionProperty::IS_PROTECTED
+			|  \ReflectionProperty::IS_PUBLIC;
+	}
+	
+	/**
+	 *  Just a synonym to allFlags()
+	 * @return int
+	 */
+	static function anyVisibility() {
+		return self::allFlags();
+	}
+		
+
+	/**
+	 * Retrieve names and values from the attributes of a object, as a map.
+	 * 
+	 * @param object	$obj				The object.
+	 *
+	 * @param int		$visibilityFlags 	Filter visibility flags. Can be added.
+	 *										Example: RTTI::IS_PRIVATE | RTTI::IS_PROTECTED
+	 *
+	 * @param string	$getterPrefix		The prefix for getter public methods (defaults to 'get').
+	 *
+	 * @param bool		$useCamelCase		If true, private and protected attributes will be accessed
+	 * 										by camelCase public methods (default true).
+	 *
+	 * @return array
+	 */
+	static function getAttributes( $obj, $visibilityFlags, $getterPrefix = 'get', $useCamelCase = true ) {
+		if ( ! isset( $obj ) ) {
+			return array();
+		}
+		$attributes = array();
+		$reflectionObject = new \ReflectionObject( $obj );
+		$currentClass = new \ReflectionClass( $obj );
+		
+		while ( $currentClass !== false && ! $currentClass->isInterface() ) {	
+		
+			$properties = $currentClass->getProperties( $visibilityFlags );
+				
+			foreach ( $properties as $property ) {
+				
+				$attributeName = $property->getName();
+				$methodName = $getterPrefix . ( $useCamelCase ? self::mb_ucfirst( $attributeName ) : $attributeName );
+				
+				if ( $property->isPrivate() || $property->isProtected() ) {
+				
+					if ( $reflectionObject->hasMethod( $methodName ) ) {
+						
+						$method = $reflectionObject->getMethod( $methodName );
+						if ( $method->isPublic() ) {
+							$attributes[ $attributeName ] = $method->invoke( $obj );
+						}
+					}
+					
+				} else { // public
+					
+					try {
+						$attributes[ $attributeName ] = $obj->{ $attributeName };
+					} catch ( \Exception $e ) {
+						// Ignore
+					}
+				}
+				
+			}
+			
+			// No properties? -> try to retrieve only public properties
+			if ( count( $properties ) < 1 ) {
+				$properties = get_object_vars( $obj );
+				foreach ( $properties as $k => $v ) {
+					$attributes[ $k ] = $v;
+				}
+			}
+			
+			$currentClass = $currentClass->getParentClass();
+		}
+		return $attributes;		
+	}		
+	
+	
+	
+	/**
+	 * Retrieve names and values from the private attributes of a object, as a map.
+	 * This method has been kept for backward compatibility.
+	 * 
+	 * @param object	$obj				The object.
+	 * @param string	$getterPrefix		The prefix for getter public methods (defaults to 'get').
+	 * @param bool		$useCamelCase		If true, private attributes will be accessed by camelCase public methods (default true).
+	 * @return array
+	 */
+	static function getPrivateAttributes( $obj, $getterPrefix = 'get', $useCamelCase = true ) {
+		return self::getAttributes( $obj, self::IS_PRIVATE, $getterPrefix, $useCamelCase );
+	}
+	
+	
+	
+	/**
+	 * Set the attribute values of a object.
+	 * 
+	 * @param array		$map				A map with the attribute names and values to be changed.
+	 *
+	 * @param object	$obj				The object to be changed.
+	 *
+	 * @param int		$visibilityFlags 	Filter visibility flags. Can be added.
+	 * 										Example: RTTI::IS_PRIVATE | RTTI::IS_PROTECTED
+	 *
+	 * @param string	$setterPrefix		The prefix for setter public methods (defaults to 'set').
+	 *
+	 * @param bool		$useCamelCase		If true, private and protected attributes will be set by
+	 *										camelCase public methods (default true).
+	 */	
+	static function setAttributes(
+		array $map, &$obj, $visibilityFlags, $setterPrefix = 'set', $useCamelCase = true
+		) {
+		$reflectionObject = new \ReflectionObject( $obj );
+		$currentClass = new \ReflectionClass( $obj );
+		while ( $currentClass !== false && ! $currentClass->isInterface() ) {
+		
+			$properties = $currentClass->getProperties( $visibilityFlags );
+				
+			foreach ( $properties as $property ) {
+				
+				$attributeName = $property->getName();
+				$methodName = $setterPrefix . ( $useCamelCase ? self::mb_ucfirst( $attributeName ) : $attributeName );
+				
+				if ( $property->isPrivate() || $property->isProtected() ) {
+					
+					if ( $reflectionObject->hasMethod( $methodName ) ) {
+						
+						$method = $reflectionObject->getMethod( $methodName );
+						if ( $method->isPublic() && array_key_exists( $attributeName, $map ) ) {
+							$method->invoke( $obj, $map[ $attributeName ] );
+						}
+					}
+					
+				} else { // public
+					try {
+						if ( array_key_exists( $attributeName, $map ) ) {
+							$obj->{ $attributeName } = $map[ $attributeName ];
+						}
+					} catch ( \Exception $e ) {
+						// Ignore
+					}
+				}
+			}
+			
+			// No properties? -> try to retrieve only public properties
+			if ( count( $properties ) < 1 ) {
+				$properties = get_object_vars( $obj );
+				foreach ( $properties as $attributeName => $v ) {
+					if ( array_key_exists( $attributeName, $map ) ) { 
+						try {
+							$obj->{ $attributeName } = $map[ $attributeName ];
+						} catch ( \Exception $e ) {
+							// Ignore
+						}
+					}
+				}
+			}			
+			
+			$currentClass = $currentClass->getParentClass();
+		}
+	}
+
+	/**
+	 * Set the attribute values of a object.
+	 * This method has been kept for backward compatibility.
+	 * 
+	 * @param array		$map				A map with the attribute names and values to be changed.
+	 *
+	 * @param object	$obj				The object to be changed.
+	 *
+	 * @param string	$setterPrefix		The prefix for setter public methods (defaults to 'set').
+	 *
+	 * @param bool		$useCamelCase		If true, private and protected attributes will be set by
+	 *										camelCase public methods (default true).
+	 */	
+	static function setPrivateAttributes(
+		array $map, &$obj, $setterPrefix = 'set', $useCamelCase = true
+		) {
+		self::setAttributes( $map, $obj, \RTTI::IS_PRIVATE, $setterPrefix, $useCamelCase );
+	}
+
+	// Multibyte version of ucfirst()
+	private static function mb_ucfirst( $str ) {
+		if ( ! is_string( $str ) || '' === $str) { return ''; }
+		$first = mb_strtoupper( mb_substr( $str, 0, 1 ) );
+		if ( 1 === mb_strlen( $str ) ) { return $first; }
+		return $first . mb_substr( $str, 1 );
+	}	
+}
+?>
